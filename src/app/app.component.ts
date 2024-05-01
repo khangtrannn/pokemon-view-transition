1a2abb0a69278cb2bfd10cba403bca2378a019e6
Khang Tran <khangtrann8198@gmail.com>
2024-05-01 12:18:05 +0700

    update readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<router-outlet />`,
+  template: `
+    <router-outlet />
+    <div class="tw-absolute tw-flex tw-bottom-[30px] tw-w-full tw-justify-center">
+      <p>Made by <span class="tw-font-semibold">Khang Tran</span> with ❤️</p>
+    </div>
+  `,
   imports: [RouterOutlet],
 })
 export class AppComponent {}
